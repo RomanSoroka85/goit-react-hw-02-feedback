89f6ae10305534035a4c066eafc2de03fc06f54a
RomanSoroka85 <patriotroma9@gmail.com>
2021-01-10 18:54:47 +0200

    hw2/1

--- src/Components/user/User.js
+++ src/Components/user/User.js
@@ -7,61 +7,103 @@ export default class User extends Component {
   // static defaultProps = {};
 
   state = {
-    good: this.props.initialValue,
-    neutral: this.props.initialValue,
-    bad: this.props.initialValue,
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
 
-  onGood = () => {
-    this.setState({ good: this.state.good + 1 });
+  onHandleClick = (e) => {
+    const { name } = e.target;
+    this.setState((prevState) => ({ [name]: prevState[name] + 1 }));
   };
-  onNeutral = () => {
-    this.setState({ neutral: this.state.neutral + 1 });
+  getTotalFeebback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
   };
 
-  onBad = () => {
-    this.setState({ bad: this.state.bad + 1 });
+  getPositiveFeedback = () => {
+    return (this.state.good * 100) / this.getTotalFeebback();
   };
   render() {
-    const g = this.state.good;
-    const n = this.state.neutral;
-    const b = this.state.bad;
+    const { good, neutral, bad } = this.state;
 
     return (
+      // <>
+      //   <div className={s.profile}>
+      //     <div className={s.description}>
+      //       <h3> Pleace leave feedback</h3>
+      //       <button type="button" onClick={this.onGood} className={s.name}>
+      //         Good
+      //       </button>
+      //       <button type="button" onClick={this.onNeutral} className={s.name}>
+      //         Neutral
+      //       </button>
+      //       <button type="button" onClick={this.onBad} className={s.name}>
+      //         Bad
+      //       </button>
+      //       <p className={s.statistics}>Statistics</p>
+      //       {!!g && <p>Good:{g}</p>}
+      //       {!!n && <p>Neutral:{n}</p>}
+      //       {!!b && <p>Bad:{b}</p>}
+
+      //       {!!(g + n + b)  && <p>Total:{g + n + b}</p>}
+
+      //       {!!(g + n + b) && (
+      //         <p className={s.statistics}>
+      //           Positive feedback {((g / (g + n + b)) * 100).toFixed(2)}%
+      //         </p>
+      //       )}
+      //     </div>
+      //   </div>
+      // </>
       <>
         <div className={s.profile}>
           <div className={s.description}>
             <h3> Pleace leave feedback</h3>
-            <button type="button" onClick={this.onGood} className={s.name}>
+            <button
+              type="button"
+              name="good"
+              onClick={this.onHandleClick}
+              className={s.name}
+            >
               Good
             </button>
-            <button type="button" onClick={this.onNeutral} className={s.name}>
-              Neutral
-            </button>
-            <button type="button" onClick={this.onBad} className={s.name}>
-              Bad
+            <button
+              type="button"
+              name="neutral"
+              onClick={this.onHandleClick}
+              className={s.name}
+            >
+              Neutral{" "}
+            </button>{" "}
+            <button
+              type="button"
+              name="bad"
+              onClick={this.onHandleClick}
+              className={s.name}
+            >
+              Bad{" "}
             </button>
             <p className={s.statistics}>Statistics</p>
-            {!!g && <p>Good:{g}</p>}
-            {!!n && <p>Neutral:{n}</p>}
-            {!!b && <p>Bad:{b}</p>}
-
-            {!!g + n + b && (
-              <p>
-                Total:{g + n + b}
-              </p>
-            )}
-
-            {!!g + n + b && (
-              <p className={s.statistics}>
-                Positive feedback{" "}
-                {(
-                  (g /
-                    (g + n + b)) *
-                  100
-                ).toFixed(2)}
-                %
-              </p>
+            {!!this.getTotalFeebback() && (
+              <ul className={s.feedBack}>
+                <li>
+                  <span>Good: {good}</span>
+                </li>
+                <li>
+                  <span>Neutral: {neutral}</span>
+                </li>
+                <li>
+                  <span>Bad: {bad}</span>
+                </li>
+                <li>
+                  <span>Total: {this.getTotalFeebback()}</span>
+                </li>
+                <li>
+                  <span>
+                    Positive feedback: {this.getPositiveFeedback().toFixed(2)}%
+                  </span>
+                </li>
+              </ul>
             )}
           </div>
         </div>
